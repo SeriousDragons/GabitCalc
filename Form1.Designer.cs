4148b9207c4a0c84f555b899262a078e6c7f7ff3
SeriousDragons <33166648+SeriousDragons@users.noreply.github.com>
2020-05-11 17:51:06 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace GambitCalculatorApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.InfamyRank = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24.25F);
+            this.button1.Location = new System.Drawing.Point(13, 356);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(246, 55);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "RUN";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // InfamyRank
+            // 
+            this.InfamyRank.FormattingEnabled = true;
+            this.InfamyRank.Items.AddRange(new object[] {
+            "Guardian 1",
+            "Guardian 2",
+            "Guardian 3",
+            "Brave 1",
+            "Brave 2",
+            "Brave 3",
+            "Heroic 1",
+            "Heroic 2",
+            "Heroic 3",
+            "Fabled 1",
+            "Fabled 2",
+            "Fabled 3",
+            "Mythic 1",
+            "Mythic 2",
+            "Mythic 3",
+            "Legend"});
+            this.InfamyRank.Location = new System.Drawing.Point(13, 13);
+            this.InfamyRank.Name = "InfamyRank";
+            this.InfamyRank.Size = new System.Drawing.Size(228, 212);
+            this.InfamyRank.TabIndex = 1;
+            this.InfamyRank.SelectedIndexChanged += new System.EventHandler(this.InfamyRank_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 232);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(229, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "Which Gambit Rank are you?";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(560, 232);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(228, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "Which Gambit Rank do you wish to become?";
+            this.textBox2.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(581, 325);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(207, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "This will become how many wins you need";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(355, 383);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(433, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "This will become how many losses you can have that will increase your points up t" +
+    "o legend";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(584, 342);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(203, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(584, 400);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(202, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(73, 428);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(655, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Note: This application assumes you have the minimum amount of points for the rank" +
+    " you are, so you may require less wins than calculated";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Guardian 2",
+            "Guardian 3",
+            "Brave 1",
+            "Brave 2",
+            "Brave 3",
+            "Heroic 1",
+            "Heroic 2",
+            "Heroic 3",
+            "Fabled 1",
+            "Fabled 2",
+            "Fabled 3",
+            "Mythic 1",
+            "Mythic 2",
+            "Mythic 3",
+            "Legend",
+            "Max"});
+            this.listBox1.Location = new System.Drawing.Point(560, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(228, 212);
+            this.listBox1.TabIndex = 10;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.ListBox1_SelectedIndexChanged);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.progressBar1.Location = new System.Drawing.Point(248, 13);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(306, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 11;
+            this.progressBar1.Visible = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(13, 342);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(98, 17);
+            this.checkBox1.TabIndex = 12;
+            this.checkBox1.Text = "List Points too?";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(118, 342);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(141, 20);
+            this.textBox5.TabIndex = 13;
+            this.textBox5.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(179)))), ((int)(((byte)(79)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.InfamyRank);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Gambit Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox InfamyRank;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GambitCalculatorApp
+{
+    public partial class Form1 : Form
+    {
+        public int wins;
+        public int losses;
+        public string A;
+        public string B;
+        public int points;
+        public int wantedpoints;
+        public int Guardian; public int Guardian2; public int Guardian3;
+        public int Brave; public int Brave2; public int Brave3;
+        public int Heroic; public int Heroic2; public int Heroic3;
+        public int Fabled; public int Fabled2; public int Fabled3;
+        public int Mythic; public int Mythic2; public int Mythic3;
+        public int Legend; public int Max;
+        public string over;
+        public bool C;
+        public bool D;
+        public string L;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label3.Text = "Note: This calculator isn't perfect and will often overcalculate the wins and losses. It also doesn't account for streaks.";
+        }
+
+        private void InfamyRank_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        A = InfamyRank.SelectedItem.ToString();
+        }
+
+        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            B = listBox1.SelectedItem.ToString();
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        static bool StringChecker(string String1,string String2)
+        {
+            if(String1 == String2) { return true; }
+            else { return false; }
+        }
+        static bool IntChecker(int int1, int int2)
+        {
+            if (int1 >= int2)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            progressBar1.Visible = true;
+            Thread.Sleep(100);
+            wins = 0; losses = 0;
+            Guardian = 0; Guardian2 = 250; Guardian3 = 600;
+            Brave = 1000; Brave2 = 1450; Brave3 = 1950;
+            Heroic = 2500; Heroic2 = 3100; Heroic3 = 3750;
+            Fabled = 4500;  Fabled2 = 5350; Fabled3 = 6350;
+            Mythic = 7500; Mythic2 = 8800; Mythic3 = 10300;
+            Legend = 12000; Max = 15000;
+            points = 0; wantedpoints = 0;
+            progressBar1.Value = 10;
+            Thread.Sleep(100);
+            if(StringChecker(A,"Guardian 1"))
+            {
+                points = Guardian;
+            }
+            if (StringChecker(A, "Guardian 2"))
+            {
+                points = Guardian2;
+            }
+            if (StringChecker(A, "Guardian 3"))
+            {
+                points = Guardian3;
+            }
+            if (StringChecker(A, "Brave 1"))
+            {
+                points = Brave;
+            }
+            if (StringChecker(A, "Brave 2"))
+            {
+                points = Brave2;
+            }
+            if (StringChecker(A, "Brave 3"))
+            {
+                points = Brave3;
+            }
+            if (StringChecker(A, "Heroic 1"))
+            {
+                points = Heroic;
+            }
+            if (StringChecker(A, "Heroic 2"))
+            {
+                points = Heroic2;
+            }
+            if (StringChecker(A, "Heroic 3"))
+            {
+                points = Heroic3;
+            }
+            if (StringChecker(A, "Fabled 1"))
+            {
+                points = Fabled;
+            }
+            if (StringChecker(A, "Fabled 2"))
+            {
+                points = Fabled2;
+            }
+            if (StringChecker(A, "Fabled 3"))
+            {
+                points = Fabled3;
+            }
+            if (StringChecker(A, "Mythic 1"))
+            {
+                points = Mythic;
+            }
+            if (StringChecker(A, "Mythic 2"))
+            {
+                points = Mythic2;
+            }
+            if (StringChecker(A, "Mythic 3"))
+            {
+                points = Mythic3;
+            }
+            if (StringChecker(A, "Legend"))
+            {
+                points = Legend;
+            }
+            progressBar1.Value = 40;
+            Thread.Sleep(100);
+            if (StringChecker(B, "Guardian 1"))
+            {
+               wantedpoints = Guardian;
+            }
+            if (StringChecker(B, "Guardian 2"))
+            {
+               wantedpoints = Guardian2;
+            }
+            if (StringChecker(B, "Guardian 3"))
+            {
+               wantedpoints = Guardian3;
+            }
+            if (StringChecker(B, "Brave 1"))
+            {
+               wantedpoints = Brave;
+            }
+            if (StringChecker(B, "Brave 2"))
+            {
+               wantedpoints = Brave2;
+            }
+            if (StringChecker(B, "Brave 3"))
+            {
+               wantedpoints = Brave3;
+            }
+            if (StringChecker(B, "Heroic 1"))
+            {
+               wantedpoints = Heroic;
+            }
+            if (StringChecker(B, "Heroic 2"))
+            {
+               wantedpoints = Heroic2;
+            }
+            if (StringChecker(B, "Heroic 3"))
+            {
+               wantedpoints = Heroic3;
+            }
+            if (StringChecker(B, "Fabled 1"))
+            {
+               wantedpoints = Fabled;
+            }
+            if (StringChecker(B, "Fabled 2"))
+            {
+               wantedpoints = Fabled2;
+            }
+            if (StringChecker(B, "Fabled 3"))
+            {
+               wantedpoints = Fabled3;
+            }
+            if (StringChecker(B, "Mythic 1"))
+            {
+               wantedpoints = Mythic;
+            }
+            if (StringChecker(B, "Mythic 2"))
+            {
+               wantedpoints = Mythic2;
+            }
+            if (StringChecker(B, "Mythic 3"))
+            {
+               wantedpoints = Mythic3;
+            }
+            if (StringChecker(B, "Legend"))
+            {
+               wantedpoints = Legend;
+            }
+            if (StringChecker(B, "Max"))
+            {
+               wantedpoints = Max;
+            }
+            textBox4.Text = "" + wantedpoints;
+            progressBar1.Value = 75;
+            Thread.Sleep(100);
+            for ( ; IntChecker(points, wantedpoints);)
+            {
+                points = points + 100;
+                if (points < 7500)
+                {
+                    losses = losses + 2;
+                }
+                else
+                {
+                    if (points < 12000)
+                    {
+                        losses = losses + 4;
+                    }
+                }
+                wins = wins + 1;
+            }
+            progressBar1.Value = 100;
+            Thread.Sleep(200);
+            if (checkBox1.Checked)
+            {
+                textBox5.Visible = true;
+                textBox5.Text = "Points: " + points;
+            }
+            else
+            {
+                textBox5.Visible = false;
+                textBox5.Text = "";
+            }
+            textBox3.Text = "Wins: " + wins;
+            textBox4.Text = "Losses: " + losses + " " + over;
+            progressBar1.Visible = false;
+        }
+
+       
+    }
+}
